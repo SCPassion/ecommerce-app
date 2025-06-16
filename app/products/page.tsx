2da127a5f2fe398355f629182d19fbe17a01c950
SCPassion <bernard.cryptohk@gmail.com>
2025-06-16 21:38:21 +0200

    Update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,16 @@
-export default function ProductsPage() {
+import ProductList from "@/components/ProductList";
+import { stripe } from "@/lib/stripe";
+
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"], // want the default price details
+  });
   return (
-    <div>
-      <h1>Products</h1>
-      <p>List of products will be displayed here.</p>
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All Products
+      </h1>
+      <ProductList products={products.data} />
     </div>
   );
 }
